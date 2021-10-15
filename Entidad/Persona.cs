387f85a70e34f1e4d7a2113ef070e8c261fc7035
djosedonado <89169531+djosedonado@users.noreply.github.com>
2021-10-14 23:21:34 -0500

    Agregar archivos de proyecto.

---
+++ Entidad/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    public class Persona
+    {
+        public Persona()
+        {
+            Edad = 0;
+        }
+
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidó { get; set; }
+        public string Sexo { get; set; }
+        public int Edad { get; set; }
+        public string Telefono { get; set; }
+
+
+    }
+
+}
