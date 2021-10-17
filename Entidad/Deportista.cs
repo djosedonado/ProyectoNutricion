296a029f5370659a052baedfb81f52943ae0cd1d
albertosep27@gmail.com <albertosep27@gmail.com>
2021-10-17 12:09:04 -0500

    Creacion de la clases Deportista heredando de persona

---
+++ Entidad/Deportista.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    public class Deportista : Persona
+    {
+        public Deportista()
+        {
+
+        }
+        public string TipoDeporte { get; set; }
+        public string Peso { get; set; }
+        public string Altura { get; set; }
+        public string HorasPracticas { get; set; }
+    }
+}
