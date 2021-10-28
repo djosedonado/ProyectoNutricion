17525ee47302c5a5279ef575fe81484e1e509da2
djosedonado <89169531+djosedonado@users.noreply.github.com>
2021-10-28 01:37:38 -0500

    Actualizacion Interfaz Grafica Energetica

--- Presentacion/TasaMetablistica.Designer.cs
+++ Presentacion/TasaMetablistica.Designer.cs
@@ -48,14 +48,34 @@ namespace Presentacion
             this.textTasaIdentificacion = new System.Windows.Forms.TextBox();
             this.LabelIdentificacion = new System.Windows.Forms.Label();
             this.GraficaBarraEnergetica = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Lunes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Martes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Miercoles = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Jueves = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Viernes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sabado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Domingo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textEntrenamientoFuerza = new System.Windows.Forms.TextBox();
+            this.TextEntrenamientoAerobicos = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.PanelFondoTasa.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.GraficaBarraEnergetica)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // PanelFondoTasa
             // 
             this.PanelFondoTasa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(182)))), ((int)(((byte)(195)))));
+            this.PanelFondoTasa.Controls.Add(this.button1);
+            this.PanelFondoTasa.Controls.Add(this.label2);
+            this.PanelFondoTasa.Controls.Add(this.label1);
+            this.PanelFondoTasa.Controls.Add(this.TextEntrenamientoAerobicos);
+            this.PanelFondoTasa.Controls.Add(this.textEntrenamientoFuerza);
+            this.PanelFondoTasa.Controls.Add(this.dataGridView2);
             this.PanelFondoTasa.Controls.Add(this.GraficaBarraEnergetica);
             this.PanelFondoTasa.Controls.Add(this.LabelIdentificacion);
             this.PanelFondoTasa.Controls.Add(this.textTasaIdentificacion);
@@ -142,22 +162,22 @@ namespace Presentacion
             series1.Name = "Lunes";
             series2.ChartArea = "GRAFO";
             series2.Legend = "Legend1";
-            series2.Name = "Series2";
+            series2.Name = "Martes";
             series3.ChartArea = "GRAFO";
             series3.Legend = "Legend1";
-            series3.Name = "Series3";
+            series3.Name = "Miercoles";
             series4.ChartArea = "GRAFO";
             series4.Legend = "Legend1";
-            series4.Name = "Series4";
+            series4.Name = "Jueves";
             series5.ChartArea = "GRAFO";
             series5.Legend = "Legend1";
-            series5.Name = "Series5";
+            series5.Name = "Viernes";
             series6.ChartArea = "GRAFO";
             series6.Legend = "Legend1";
-            series6.Name = "Series6";
+            series6.Name = "Sabado";
             series7.ChartArea = "GRAFO";
             series7.Legend = "Legend1";
-            series7.Name = "Series7";
+            series7.Name = "Domingo";
             this.GraficaBarraEnergetica.Series.Add(series1);
             this.GraficaBarraEnergetica.Series.Add(series2);
             this.GraficaBarraEnergetica.Series.Add(series3);
@@ -169,6 +189,102 @@ namespace Presentacion
             this.GraficaBarraEnergetica.TabIndex = 4;
             this.GraficaBarraEnergetica.Text = "chart1";
             this.GraficaBarraEnergetica.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Lunes,
+            this.Martes,
+            this.Miercoles,
+            this.Jueves,
+            this.Viernes,
+            this.Sabado,
+            this.Domingo});
+            this.dataGridView2.Location = new System.Drawing.Point(33, 364);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(740, 211);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // Lunes
+            // 
+            this.Lunes.HeaderText = "Lunes";
+            this.Lunes.Name = "Lunes";
+            // 
+            // Martes
+            // 
+            this.Martes.HeaderText = "Martes";
+            this.Martes.Name = "Martes";
+            // 
+            // Miercoles
+            // 
+            this.Miercoles.HeaderText = "Miercoles";
+            this.Miercoles.Name = "Miercoles";
+            // 
+            // Jueves
+            // 
+            this.Jueves.HeaderText = "Jueves";
+            this.Jueves.Name = "Jueves";
+            // 
+            // Viernes
+            // 
+            this.Viernes.HeaderText = "Viernes";
+            this.Viernes.Name = "Viernes";
+            // 
+            // Sabado
+            // 
+            this.Sabado.HeaderText = "Sabado";
+            this.Sabado.Name = "Sabado";
+            // 
+            // Domingo
+            // 
+            this.Domingo.HeaderText = "Domingo";
+            this.Domingo.Name = "Domingo";
+            // 
+            // textEntrenamientoFuerza
+            // 
+            this.textEntrenamientoFuerza.Location = new System.Drawing.Point(77, 304);
+            this.textEntrenamientoFuerza.Name = "textEntrenamientoFuerza";
+            this.textEntrenamientoFuerza.Size = new System.Drawing.Size(99, 20);
+            this.textEntrenamientoFuerza.TabIndex = 6;
+            // 
+            // TextEntrenamientoAerobicos
+            // 
+            this.TextEntrenamientoAerobicos.Location = new System.Drawing.Point(267, 304);
+            this.TextEntrenamientoAerobicos.Name = "TextEntrenamientoAerobicos";
+            this.TextEntrenamientoAerobicos.Size = new System.Drawing.Size(100, 20);
+            this.TextEntrenamientoAerobicos.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(65, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Entrenamiento de Fuerza";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(254, 286);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Entrenamineto Aerobicos";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(410, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // TasaMetablistica
             // 
@@ -183,6 +299,7 @@ namespace Presentacion
             this.PanelFondoTasa.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.GraficaBarraEnergetica)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -199,5 +316,18 @@ namespace Presentacion
         private System.Windows.Forms.Label LabelIdentificacion;
         private System.Windows.Forms.TextBox textTasaIdentificacion;
         private System.Windows.Forms.Button botonBuscarIdentificacion;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Lunes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Martes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Miercoles;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Jueves;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Viernes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sabado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Domingo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextEntrenamientoAerobicos;
+        private System.Windows.Forms.TextBox textEntrenamientoFuerza;
     }
 }
