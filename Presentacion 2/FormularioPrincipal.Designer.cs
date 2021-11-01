59e5fde9d36a343dbb7f123bcb152b060b1275f1
djosedonado <djosedonado@unicesar.edu.co>
2021-11-01 04:39:49 -0500

    SegundoFormulario

---
+++ Presentacion 2/FormularioPrincipal.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace Presentacion_2
+{
+    partial class FormularioPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormularioPrincipal));
+            this.BotonRestaurar = new Bunifu.UI.WinForms.BunifuImageButton();
+            this.BotonMinimizar = new Bunifu.UI.WinForms.BunifuImageButton();
+            this.BotonMaximizar = new Bunifu.UI.WinForms.BunifuImageButton();
+            this.BotonCerrar = new Bunifu.UI.WinForms.BunifuImageButton();
+            this.bunifuPanel1 = new Bunifu.UI.WinForms.BunifuPanel();
+            this.bunifuPanel2 = new Bunifu.UI.WinForms.BunifuPanel();
+            this.bunifuPanel3 = new Bunifu.UI.WinForms.BunifuPanel();
+            this.bunifuPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BotonRestaurar
+            // 
+            this.BotonRestaurar.ActiveImage = null;
+            this.BotonRestaurar.AllowAnimations = true;
+            this.BotonRestaurar.AllowBuffering = false;
+            this.BotonRestaurar.AllowToggling = false;
+            this.BotonRestaurar.AllowZooming = true;
+            this.BotonRestaurar.AllowZoomingOnFocus = false;
+            this.BotonRestaurar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(0)))), ((int)(((byte)(224)))));
+            this.BotonRestaurar.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.BotonRestaurar.ErrorImage = ((System.Drawing.Image)(resources.GetObject("BotonRestaurar.ErrorImage")));
+            this.BotonRestaurar.FadeWhenInactive = false;
+            this.BotonRestaurar.Flip = Bunifu.UI.WinForms.BunifuImageButton.FlipOrientation.Normal;
+            this.BotonRestaurar.Image = global::Presentacion_2.Properties.Resources.Reducir;
+            this.BotonRestaurar.ImageActive = null;
+            this.BotonRestaurar.ImageLocation = null;
+            this.BotonRestaurar.ImageMargin = 15;
+            this.BotonRestaurar.ImageSize = new System.Drawing.Size(20, 20);
+            this.BotonRestaurar.ImageZoomSize = new System.Drawing.Size(35, 35);
+            this.BotonRestaurar.InitialImage = ((System.Drawing.Image)(resources.GetObject("BotonRestaurar.InitialImage")));
+            this.BotonRestaurar.Location = new System.Drawing.Point(721, 3);
+            this.BotonRestaurar.Name = "BotonRestaurar";
+            this.BotonRestaurar.Rotation = 0;
+            this.BotonRestaurar.ShowActiveImage = true;
+            this.BotonRestaurar.ShowCursorChanges = true;
+            this.BotonRestaurar.ShowImageBorders = true;
+            this.BotonRestaurar.ShowSizeMarkers = false;
+            this.BotonRestaurar.Size = new System.Drawing.Size(35, 35);
+            this.BotonRestaurar.TabIndex = 3;
+            this.BotonRestaurar.ToolTipText = "";
+            this.BotonRestaurar.Visible = false;
+            this.BotonRestaurar.WaitOnLoad = false;
+            this.BotonRestaurar.Zoom = 15;
+            this.BotonRestaurar.ZoomSpeed = 10;
+            this.BotonRestaurar.Click += new System.EventHandler(this.BotonRestaurar_Click);
+            // 
+            // BotonMinimizar
+            // 
+            this.BotonMinimizar.ActiveImage = null;
+            this.BotonMinimizar.AllowAnimations = true;
+            this.BotonMinimizar.AllowBuffering = false;
+            this.BotonMinimizar.AllowToggling = false;
+            this.BotonMinimizar.AllowZooming = true;
+            this.BotonMinimizar.AllowZoomingOnFocus = false;
+            this.BotonMinimizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(0)))), ((int)(((byte)(224)))));
+            this.BotonMinimizar.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.BotonMinimizar.ErrorImage = ((System.Drawing.Image)(resources.GetObject("BotonMinimizar.ErrorImage")));
+            this.BotonMinimizar.FadeWhenInactive = false;
+            this.BotonMinimizar.Flip = Bunifu.UI.WinForms.BunifuImageButton.FlipOrientation.Normal;
+            this.BotonMinimizar.Image = global::Presentacion_2.Properties.Resources.Minimizar;
+            this.BotonMinimizar.ImageActive = null;
+            this.BotonMinimizar.ImageLocation = null;
+            this.BotonMinimizar.ImageMargin = 15;
+            this.BotonMinimizar.ImageSize = new System.Drawing.Size(20, 20);
+            this.BotonMinimizar.ImageZoomSize = new System.Drawing.Size(35, 35);
+            this.BotonMinimizar.InitialImage = ((System.Drawing.Image)(resources.GetObject("BotonMinimizar.InitialImage")));
+            this.BotonMinimizar.Location = new System.Drawing.Point(680, 3);
+            this.BotonMinimizar.Name = "BotonMinimizar";
+            this.BotonMinimizar.Rotation = 0;
+            this.BotonMinimizar.ShowActiveImage = true;
+            this.BotonMinimizar.ShowCursorChanges = true;
+            this.BotonMinimizar.ShowImageBorders = true;
+            this.BotonMinimizar.ShowSizeMarkers = false;
+            this.BotonMinimizar.Size = new System.Drawing.Size(35, 35);
+            this.BotonMinimizar.TabIndex = 2;
+            this.BotonMinimizar.ToolTipText = "";
+            this.BotonMinimizar.WaitOnLoad = false;
+            this.BotonMinimizar.Zoom = 15;
+            this.BotonMinimizar.ZoomSpeed = 10;
+            this.BotonMinimizar.Click += new System.EventHandler(this.BotonMinimizar_Click);
+            // 
+            // BotonMaximizar
+            // 
+            this.BotonMaximizar.ActiveImage = null;
+            this.BotonMaximizar.AllowAnimations = true;
+            this.BotonMaximizar.AllowBuffering = false;
+            this.BotonMaximizar.AllowToggling = false;
+            this.BotonMaximizar.AllowZooming = true;
+            this.BotonMaximizar.AllowZoomingOnFocus = false;
+            this.BotonMaximizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(0)))), ((int)(((byte)(224)))));
+            this.BotonMaximizar.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.BotonMaximizar.ErrorImage = ((System.Drawing.Image)(resources.GetObject("BotonMaximizar.ErrorImage")));
+            this.BotonMaximizar.FadeWhenInactive = false;
+            this.BotonMaximizar.Flip = Bunifu.UI.WinForms.BunifuImageButton.FlipOrientation.Normal;
+            this.BotonMaximizar.Image = global::Presentacion_2.Properties.Resources.Maximizar;
+            this.BotonMaximizar.ImageActive = null;
+            this.BotonMaximizar.ImageLocation = null;
+            this.BotonMaximizar.ImageMargin = 15;
+            this.BotonMaximizar.ImageSize = new System.Drawing.Size(20, 20);
+            this.BotonMaximizar.ImageZoomSize = new System.Drawing.Size(35, 35);
+            this.BotonMaximizar.InitialImage = ((System.Drawing.Image)(resources.GetObject("BotonMaximizar.InitialImage")));
+            this.BotonMaximizar.Location = new System.Drawing.Point(721, 3);
+            this.BotonMaximizar.Name = "BotonMaximizar";
+            this.BotonMaximizar.Rotation = 0;
+            this.BotonMaximizar.ShowActiveImage = true;
+            this.BotonMaximizar.ShowCursorChanges = true;
+            this.BotonMaximizar.ShowImageBorders = true;
+            this.BotonMaximizar.ShowSizeMarkers = false;
+            this.BotonMaximizar.Size = new System.Drawing.Size(35, 35);
+            this.BotonMaximizar.TabIndex = 1;
+            this.BotonMaximizar.ToolTipText = "";
+            this.BotonMaximizar.WaitOnLoad = false;
+            this.BotonMaximizar.Zoom = 15;
+            this.BotonMaximizar.ZoomSpeed = 10;
+            this.BotonMaximizar.Click += new System.EventHandler(this.BotonMaximizar_Click);
+            // 
+            // BotonCerrar
+            // 
+            this.BotonCerrar.ActiveImage = null;
+            this.BotonCerrar.AllowAnimations = true;
+            this.BotonCerrar.AllowBuffering = false;
+            this.BotonCerrar.AllowToggling = false;
+            this.BotonCerrar.AllowZooming = true;
+            this.BotonCerrar.AllowZoomingOnFocus = false;
+            this.BotonCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(0)))), ((int)(((byte)(224)))));
+            this.BotonCerrar.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.BotonCerrar.ErrorImage = ((System.Drawing.Image)(resources.GetObject("BotonCerrar.ErrorImage")));
+            this.BotonCerrar.FadeWhenInactive = false;
+            this.BotonCerrar.Flip = Bunifu.UI.WinForms.BunifuImageButton.FlipOrientation.Normal;
+            this.BotonCerrar.Image = global::Presentacion_2.Properties.Resources.cancel_96245;
+            this.BotonCerrar.ImageActive = null;
+            this.BotonCerrar.ImageLocation = null;
+            this.BotonCerrar.ImageMargin = 15;
+            this.BotonCerrar.ImageSize = new System.Drawing.Size(20, 20);
+            this.BotonCerrar.ImageZoomSize = new System.Drawing.Size(35, 35);
+            this.BotonCerrar.InitialImage = ((System.Drawing.Image)(resources.GetObject("BotonCerrar.InitialImage")));
+            this.BotonCerrar.Location = new System.Drawing.Point(762, 3);
+            this.BotonCerrar.Name = "BotonCerrar";
+            this.BotonCerrar.Rotation = 0;
+            this.BotonCerrar.ShowActiveImage = true;
+            this.BotonCerrar.ShowCursorChanges = true;
+            this.BotonCerrar.ShowImageBorders = true;
+            this.BotonCerrar.ShowSizeMarkers = false;
+            this.BotonCerrar.Size = new System.Drawing.Size(35, 35);
+            this.BotonCerrar.TabIndex = 0;
+            this.BotonCerrar.ToolTipText = "";
+            this.BotonCerrar.WaitOnLoad = false;
+            this.BotonCerrar.Zoom = 15;
+            this.BotonCerrar.ZoomSpeed = 10;
+            this.BotonCerrar.Click += new System.EventHandler(this.BotonCerrar_Click);
+            // 
+            // bunifuPanel1
+            // 
+            this.bunifuPanel1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(0)))), ((int)(((byte)(224)))));
+            this.bunifuPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuPanel1.BackgroundImage")));
+            this.bunifuPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuPanel1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(0)))), ((int)(((byte)(224)))));
+            this.bunifuPanel1.BorderRadius = 0;
+            this.bunifuPanel1.BorderThickness = 0;
+            this.bunifuPanel1.Controls.Add(this.BotonCerrar);
+            this.bunifuPanel1.Controls.Add(this.BotonRestaurar);
+            this.bunifuPanel1.Controls.Add(this.BotonMaximizar);
+            this.bunifuPanel1.Controls.Add(this.BotonMinimizar);
+            this.bunifuPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bunifuPanel1.Location = new System.Drawing.Point(0, 0);
+            this.bunifuPanel1.Name = "bunifuPanel1";
+            this.bunifuPanel1.ShowBorders = true;
+            this.bunifuPanel1.Size = new System.Drawing.Size(800, 41);
+            this.bunifuPanel1.TabIndex = 4;
+            // 
+            // bunifuPanel2
+            // 
+            this.bunifuPanel2.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(12)))), ((int)(((byte)(247)))));
+            this.bunifuPanel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuPanel2.BackgroundImage")));
+            this.bunifuPanel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuPanel2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(12)))), ((int)(((byte)(247)))));
+            this.bunifuPanel2.BorderRadius = 0;
+            this.bunifuPanel2.BorderThickness = 0;
+            this.bunifuPanel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bunifuPanel2.Location = new System.Drawing.Point(0, 41);
+            this.bunifuPanel2.Name = "bunifuPanel2";
+            this.bunifuPanel2.ShowBorders = true;
+            this.bunifuPanel2.Size = new System.Drawing.Size(200, 409);
+            this.bunifuPanel2.TabIndex = 5;
+            // 
+            // bunifuPanel3
+            // 
+            this.bunifuPanel3.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(0)))), ((int)(((byte)(250)))));
+            this.bunifuPanel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuPanel3.BackgroundImage")));
+            this.bunifuPanel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuPanel3.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(0)))), ((int)(((byte)(250)))));
+            this.bunifuPanel3.BorderRadius = 0;
+            this.bunifuPanel3.BorderThickness = 0;
+            this.bunifuPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bunifuPanel3.Location = new System.Drawing.Point(200, 41);
+            this.bunifuPanel3.Name = "bunifuPanel3";
+            this.bunifuPanel3.ShowBorders = true;
+            this.bunifuPanel3.Size = new System.Drawing.Size(600, 409);
+            this.bunifuPanel3.TabIndex = 6;
+            // 
+            // FormularioPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bunifuPanel3);
+            this.Controls.Add(this.bunifuPanel2);
+            this.Controls.Add(this.bunifuPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormularioPrincipal";
+            this.Text = "Form1";
+            this.bunifuPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.UI.WinForms.BunifuImageButton BotonCerrar;
+        private Bunifu.UI.WinForms.BunifuImageButton BotonMaximizar;
+        private Bunifu.UI.WinForms.BunifuImageButton BotonMinimizar;
+        private Bunifu.UI.WinForms.BunifuImageButton BotonRestaurar;
+        private Bunifu.UI.WinForms.BunifuPanel bunifuPanel1;
+        private Bunifu.UI.WinForms.BunifuPanel bunifuPanel2;
+        private Bunifu.UI.WinForms.BunifuPanel bunifuPanel3;
+    }
+}
+
---
+++ Presentacion 2/FormularioPrincipal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion_2
+{
+    public partial class FormularioPrincipal : Form
+    {
+        public FormularioPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void BotonMaximizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            BotonMaximizar.Visible = false;
+            BotonRestaurar.Visible = true;
+
+        }
+
+        private void BotonMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void BotonRestaurar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            BotonRestaurar.Visible = false;
+            BotonMaximizar.Visible = true;
+        }
+
+        private void BotonCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
