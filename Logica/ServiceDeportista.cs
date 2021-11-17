ceb35d838e1220d653df0a9bad7c53d82957917c
Beto0829 <89174008+Beto0829@users.noreply.github.com>
2021-11-17 13:45:48 -0500

    SERVICE DEPORTISTA CONNECTION

--- Logica/ServiceDeportista.cs
+++ Logica/ServiceDeportista.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Datos;
+using Entidad;
 
 namespace Logica
 {
     public class ServiceDeportista
     {
+        private readonly ConnectionDB connection;
+        private readonly DeportistaRepository deportistaRepository;
+        public ServiceDeportista(string connectionDB)
+        {
+            connection = new ConnectionDB(connectionDB);
+            deportistaRepository = new DeportistaRepository(connection);
+        }
+
 
     }
 }
